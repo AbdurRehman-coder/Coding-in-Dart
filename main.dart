ea7d4176ef50da5001fb66bb6b91d25694411b12
ABDUR REHMAN <52750545+AbdurRehman-coder@users.noreply.github.com>
2021-04-16 01:46:50 +0500

    Comments addition
    Add some comment to the code to look cool, because without comment code looks like weird, isn't it...

--- main.dart
+++ main.dart
@@ -1,20 +1,23 @@
 import 'dart:math';
 import 'dart:io';
 
-
+//Function that will prompt the message and 
+// also return the value that would be
+// enter by the user
 String prompMessage(String promptText){
 print(promptText);
 String answer = stdin.readLineSync();
 return answer;
 }
-
+// Main Function
 void main(){
 String color = prompMessage('Enter the color:');
 String books = prompMessage('Enter reading books:');
 String celebrity = prompMessage('Enter celebrity');
 
 
-
+//this all can be done with a single code base,
+// which we called functions...
 /*print('Enter your color:');
 String color = stdin.readLineSync();
 
@@ -26,7 +29,8 @@ String books = stdin.readLineSync();
 print('Enter the name of celebrity');
 String celebrity = stdin.readLineSync();
 */
-print('He likes ${color} color');
+ // output to the console with interpolation
+print('He likes ${color} color'); 
 print('${books} is really interesting book');
 print('${celebrity} is also read ${books} when published..');
 
