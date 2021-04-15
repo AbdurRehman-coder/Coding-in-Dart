1ff4e932bc78aac43018866af85d911bef1d1f59
ABDUR REHMAN <>
2021-04-15 20:30:10 +0000

    correct the String message in print, to look like cool..

--- main.dart
+++ main.dart
@@ -1,4 +1,35 @@
+import 'dart:math';
+import 'dart:io';
+
+
+String prompMessage(String promptText){
+print(promptText);
+String answer = stdin.readLineSync();
+return answer;
+}
+
 void main(){
-List<int> myValues = [2,4,66,45,34,1, 65];
-print("the values of list is: ${myValues}");
+String color = prompMessage('Enter the color:');
+String books = prompMessage('Enter reading books:');
+String celebrity = prompMessage('Enter celebrity');
+
+
+
+/*print('Enter your color:');
+String color = stdin.readLineSync();
+
+
+print('Enter your books');
+String books = stdin.readLineSync();
+
+
+print('Enter the name of celebrity');
+String celebrity = stdin.readLineSync();
+*/
+print('He likes ${color} color');
+print('${books} is really interesting book');
+print('${celebrity} is also read ${books} when published..');
+
+
+
 }
